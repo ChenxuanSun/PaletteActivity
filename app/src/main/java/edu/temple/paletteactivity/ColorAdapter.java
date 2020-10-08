a7375d97d7c9f63aed37d692393631424378bbdd
ChenxuanSun <tug81816@temple.edu>
2020-10-08 08:59:59 +0800

    I connect the PaletteActivity with CanvasActivity use Intent, then I use method putExtra to put the information to my CanvasActivity class. After that activity start.

--- app/src/main/java/edu/temple/paletteactivity/ColorAdapter.java
+++ app/src/main/java/edu/temple/paletteactivity/ColorAdapter.java
@@ -1,23 +1,33 @@
 package edu.temple.paletteactivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ColorAdapter extends BaseAdapter {
-    ArrayList<String> items;
+    Context context;
+    String colors[];
 
+     int [] AddColors = {Color.TRANSPARENT,Color.MAGENTA,Color.CYAN,Color.DKGRAY,Color.LTGRAY,Color.GRAY,Color.WHITE,Color.BLACK,Color.BLUE,Color.GREEN,Color.YELLOW,Color.RED};
 
+    public ColorAdapter(Context context, String[] colors){
+        this.context = context;
+        this.colors = colors;
+
+    }
     @Override
     public int getCount() {
-        return items.size();
+        return colors.length;
     }
 
     @Override
     public Object getItem(int i) {
-        return items.get(i);
+        return colors[i];
     }
 
     @Override
@@ -26,7 +36,22 @@ public class ColorAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View view, ViewGroup parent) {
+
+
+        TextView textView;
+        if(view == null){
+            textView = new TextView(context);
+        }else{
+            textView = (TextView) view;
+        }
+        //set up textView
+        textView.setPadding(20,20,20,20);
+        textView.setText(getItem(i).toString());
+        textView.setBackgroundColor(AddColors[i % AddColors.length]);
+        return textView;
+    }
+    public View getDropDownView(int i, View view, ViewGroup parent){
+        return getView(i,view,parent);
     }
 }
--- app/src/main/java/edu/temple/paletteactivity/PaletteActivity.java
+++ app/src/main/java/edu/temple/paletteactivity/PaletteActivity.java
@@ -2,13 +2,41 @@ package edu.temple.paletteactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
+    GridView gridView;
+    public static final String colorSelected = "Value";
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar()!= null)
+            getSupportActionBar().setTitle("PaletteActivity");
+
+
+    gridView = (GridView) findViewById(R.id.gridviewid);
+
+    String [] colors = {"TRANSPARENT","MAGENTA","CYAN","DARK GRAY","LIGHTGRAY","GRAY","WHITE","BLACK","BLUE","GREEN","YELLOW","RED"};
+        BaseAdapter adapter = new ColorAdapter(this,colors);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                intent.putExtra(colorSelected,i);
+                startActivity(intent);
+            }
+        });
     }
 }
